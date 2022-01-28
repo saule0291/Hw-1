d94dc8867bc5c31402d1f81ede2f61cec494f256
Saule <saule.kubanychbekova.kk@gmail.com>
2022-01-28 21:35:06 +0400

    Done

---
+++ script2.js
@@ -0,0 +1,5 @@
+//2. Простые сообщения: сохраните сообщение в переменной и выведите это сообщение. Затем замените значение переменной другим сообщением и выведите новое сообщение
+let message = "Janat will learn JS";
+document.write(message+"<br>");
+message ="Saule also will learn JS"
+document.write(message+"<br>");
---
+++ script3.js
@@ -0,0 +1,4 @@
+//3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+let myName ="Saule";
+let email="saule.kubanychbekova.kk@gmail.com";
+document.write("My name is: "+ myName +"<br>"+"My email: "+ email+"<br>" );
